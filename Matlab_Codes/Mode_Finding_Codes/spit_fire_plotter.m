d048427962c474289387f65e0fc8ea83014063fe
Rodri979 <119634920+Rodri979@users.noreply.github.com>
2024-12-17 16:40:50 -0500

    Update spit_fire_plotter.m
    Documentation

--- Matlab_Codes/Mode_Finding_Codes/spit_fire_plotter.m
+++ Matlab_Codes/Mode_Finding_Codes/spit_fire_plotter.m
@@ -13,6 +13,14 @@ function spit_fire_plotter(directory, mode_data, eps_file, slice, slice_num)
 %   mode_data -> Data of the format output by mode_finder (this is the same
 %   data used to run the mode images)
 %
+%   slice -> 'x', 'y', or 'z'. Determines the plane that the slice will be a
+%   part of (if 'x' is chosen then the yz plane will be used, 'y' 
+%   then the xz plane). Slice to be input into Plot_3d_w_black_lines.m
+% 
+%   slice_num -> The number that the chosen slice dimension will be set to,
+%   for example if slice_num = 75 and slice = x then the plot will be output
+%   at x = 75. Slice_num to be put into Plot_3d_w_black_lines.m
+%
 %   eps_file -> The epsilon structure h5 file with which the field will be
 %   overlayed
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
