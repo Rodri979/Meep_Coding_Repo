f323ea9ec608743d4f876251f5fc50c13771e319
Rodri979 <119634920+Rodri979@users.noreply.github.com>
2025-03-29 15:09:17 -0400

    Adding new matlab folder

---
+++ Matlab_Codes/eps_plotter.m
@@ -0,0 +1,67 @@
+function eps_plotter(eps_data, slice, slice_num, resolution)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% eps_data -> eps h5 datafile. Input the name of your eps h5 file in quotes
+% ex: "straight_waveguide_recreate-eps-000000.00.h5"
+%
+% slice -> 'x', 'y', or 'z' slice of data you want to take. This combined with
+% slice num is like chosing a plane that goes through a specified point.
+% For example if slice = 'x' and slice_num = 37 then you are selecting the
+% yz plane which cuts through x = 37.
+%
+% slice_num -> The value to which the "slice" variable will be assigned.
+% e.g. if slice = 'y' and slice_num = 53 then the plane that satisfies 
+% y = 53 will be shown
+%
+% resolution -> The value of the resolution which the simulation was run in
+% meep. Used to calculate the size (in um) of the dimensions. If the refrence
+% dimension is not 1 um, you can change the hardcoded um dimension in the 
+% bottom switch case for now.
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+eps_data = h5read(eps_data, "/eps");
+
+switch slice
+    case 'z'
+        eps_slice = squeeze(eps_data(slice_num, :, :));
+    case 'y'
+        eps_slice = squeeze(eps_data(:, slice_num, :));
+    case 'x'
+        eps_slice = squeeze(eps_data(:, :, slice_num));
+    otherwise
+        warning('%s is an unexpected slice dimension please choose x, y, or z', slice)
+        return
+end
+
+% Visualize the 2D slice
+figure;
+imagesc(flipud(eps_slice));
+colorbar; 
+colormap(flipud(gray));
+title("Eps 2d Slice at " + slice + " = " + string(slice_num));
+axis square;
+min_2 = -1 * round((size(eps_slice,2) - 1)/(2 * resolution),1);
+max_2 = -1 * min_2;
+
+min_1 = -1 * round((size(eps_slice,1) - 1)/(2 * resolution), 1);
+max_1 = -1 * min_1;
+
+xticks(linspace(1,size(eps_slice,2),5))
+xticklabels(linspace(min_2,max_2,5))
+
+yticks(linspace(1,size(eps_slice,1),5))
+yticklabels(linspace(max_1, min_1, 5))
+
+switch slice
+    case 'z'
+        xlabel('X (\mum)');
+        ylabel('Y (\mum)');
+    case 'y'
+        xlabel('X (\mum)');
+        ylabel('Z (\mum)');
+    case 'x'
+        xlabel('Y (\mum)');
+        ylabel('Z (\mum)');
+end
+
+end
---
+++ Matlab_Codes/gif_plotter.m
@@ -0,0 +1,77 @@
+function gif_plotter(directory, eps_file, slice, slice_num, resolution, title)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   gif_plotter plots all modes in a directory. IT MUST ONLY CONTATION THE 
+%   H5 denergy FILES OUTPUT FROM A MEEP SIMULATION. It is meant to plot all
+%   files which are automatically saved (in the same directory as the h5 
+%   files) as pngs and named (thus if a mistake was made and it needs to
+%   run again delete all pngs in the directory and start over). The named
+%   png files can then be converted into a gif using the convert *.png
+%   name.gif function in bash (use magick instead of convert if convert
+%   is outdated).
+%
+%   THIS IS PART OF A PACKAGE OF CODE. IT NEEDS pull_max_min.m AND
+%   Plot_3d_w_black_lines_gif.m to run 
+%
+%   **** NOTE
+%   Plot_3d_w_black_lines_gif.m IS NOT the same as Plot_3d_w_black_lines.m
+%   
+%   directory -> input of form "directory" (MUST BE IN DOUBLE QUOTES). This
+%   is the name of the folder which has in it ONLY the h5 denergy files 
+%   output from a meep simulation
+%
+%   slice -> 'x', 'y', or 'z'. Determines the plane that the slice will be a
+%   part of (if 'x' is chosen then the yz plane will be used, 'y' 
+%   then the xz plane). Slice to be input into Plot_3d_w_black_lines.m
+% 
+%   slice_num -> The number that the chosen slice dimension will be set to,
+%   for example if slice_num = 75 and slice = x then the plot will be output
+%   at x = 75. Slice_num to be put into Plot_3d_w_black_lines.m
+%
+%   resolution -> The resolution that the simulation was run in meep
+%
+%   title -> The title on all plots. It will be the title seen on the gif.
+%
+%   eps_file -> The epsilon structure h5 file with which the field will be
+%   overlayed
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+dir_struct = dir(directory);
+pic_names = [" " " " " "];
+pic_index = 1;
+%max_field = -1;
+%min_field = 100000000000000;
+
+for dir_num = linspace(3,length(dir_struct),(length(dir_struct)-2))
+    k_name = dir_struct(dir_num).name;
+    k_dir = dir(directory + "/" + k_name);
+    for index = 1:1:length(k_dir)
+        pic_names(pic_index) = k_dir(index).name;
+        pic_index = pic_index + 1;
+    end
+end
+
+%disp(pic_names)
+
+[max_field, min_field] = pull_max_min("./" + directory + "/" + pic_names(1), "/denergy", slice, slice_num);
+   
+for graph_index = 2:1:length(pic_names)
+    [temp_max, temp_min] = pull_max_min("./" + directory + "/" + pic_names(graph_index), "/denergy", slice, slice_num);
+    if temp_max > max_field
+        max_field = temp_max;
+    end
+    if temp_min < min_field
+        min_field = temp_min;
+    end
+end
+
+for graph_index = 1:1:length(pic_names)
+    Plot_3d_w_black_lines_gif(eps_file, "/eps", "./matlab/" + directory + "/" + pic_names(graph_index), "/denergy", slice, slice_num, max_field, min_field, resolution, title)
+    fprintf(num2str(graph_index) + ": " + pic_names(graph_index))
+    fprintf("\n")
+    png_name = directory + "/" + slice + "_" + num2str(slice_num) + "_" + strrep(pic_names(graph_index),"h5","png");
+    saveas(figure(graph_index),png_name, 'png')
+end
+
+clear all
+
+end
+
